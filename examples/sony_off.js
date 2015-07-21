faade2d767ba95ca2c370b964a9fd88594fa4845
David Muzi <david.muzi@shopify.com>
2015-07-20 20:31:32 -0400

    separate test apps, better error handling

--- examples/sony_off.js
+++ examples/sony_off.js
@@ -4,11 +4,10 @@ var Device = require('../lib/device.js')
 
 var deviceSpec = JSON.parse(fs.readFileSync(path.join(__dirname, '../specs/sony_tv.json'), 'utf8'))
 var device = new Device(deviceSpec, '/dev/cu.usbserial')
-var dev = device;
 
 device.on('ready', function() {
 
-	device.powerOn(function(error, results) {
+	device.powerOff(function(error, results) {
 		
 		if (error) {
 			console.log(error)
---
+++ examples/sony_on.js
@@ -0,0 +1,19 @@
+var fs = require('fs')
+var path = require('path')
+var Device = require('../lib/device.js')
+
+var deviceSpec = JSON.parse(fs.readFileSync(path.join(__dirname, '../specs/sony_tv.json'), 'utf8'))
+var device = new Device(deviceSpec, '/dev/cu.usbserial')
+
+device.on('ready', function() {
+
+	device.powerOn(function(error, results) {
+		
+		if (error) {
+			console.log(error)
+		}
+	
+		process.exit(0)
+	});
+});
+
